5f967b6d2b2d4724db9d01aebff428026dff3a1a
Andrew Larned <andrew.larned@gmail.com>
2018-09-10 12:02:42 -0400

    Remove unneeded import

--- src/test/java/gov/healthit/chpl/aqa/stepDefinitions/ListingDetailsSteps.java
+++ src/test/java/gov/healthit/chpl/aqa/stepDefinitions/ListingDetailsSteps.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import cucumber.api.java.en.Given;
