a652f39376136e0f3f0576576de4db72538dbfc7
Andrew Larned <andrew.larned@gmail.com>
2018-09-12 09:56:12 -0400

    Take screenshot on any exception

--- src/test/java/gov/healthit/chpl/aqa/stepDefinitions/BaseSteps.java
+++ src/test/java/gov/healthit/chpl/aqa/stepDefinitions/BaseSteps.java
@@ -2,22 +2,19 @@ package gov.healthit.chpl.aqa.stepDefinitions;
 
 import java.io.File;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /** Base class for step definition files. */
 public class BaseSteps {
 
-    private WebDriver driver;
+    private EventFiringWebDriver driver;
     private WebDriverWait shortWait;
     private WebDriverWait longWait;
     private String url = System.getProperty("url");
     private String filePath = System.getProperty("filePath");
-    private String screenshotPath;
     protected static final int TIMEOUT = 30;
     protected static final int LONG_TIMEOUT = 90;
     protected static final String CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -35,26 +32,13 @@ public class BaseSteps {
             String tempDirectory = System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator + "resources";
             this.setFilePath(tempDirectory);
         }
-        screenshotPath = System.getProperty("user.dir") + File.separator + "test-output";
-    }
-
-    /**
-     * Take a screenshot.
-     * @param hash a string that will be inserted into the filename to avoid overwriting images.
-     * All "non-word characters" will be replaced with "_"
-     * @throws Exception if there is an exception
-     */
-    public void takeScreenshot(final String hash) throws Exception {
-        File scrFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
-        File outFile = new File(screenshotPath + File.separator + "failed-test-" + hash.replaceAll("\\W+", "_") + ".png");
-        FileUtils.copyFile(scrFile, outFile);
     }
 
     public WebDriver getDriver() {
         return driver;
     }
 
-    public void setDriver(final WebDriver driver) {
+    public void setDriver(final EventFiringWebDriver driver) {
         this.driver = driver;
     }
 
@@ -89,5 +73,4 @@ public class BaseSteps {
     public void setShortWait(final WebDriverWait shortWait) {
         this.shortWait = shortWait;
     }
-
 }
