022bc6ad1b89f5dc65c5c579ecded4bb62353bdf
Andrew Larned <andrew.larned@gmail.com>
2019-02-13 11:53:53 -0500

    Update src/test/java/gov/healthit/chpl/aqa/stepDefinitions/LoginSteps.java
    Co-Authored-By: iyerashwin14 <44380143+iyerashwin14@users.noreply.github.com>

--- src/test/java/gov/healthit/chpl/aqa/stepDefinitions/LoginSteps.java
+++ src/test/java/gov/healthit/chpl/aqa/stepDefinitions/LoginSteps.java
@@ -72,6 +72,8 @@ public class LoginSteps extends Base {
         } else if (role.equalsIgnoreCase("ROLE_ACB")) {
             username = roleAcbUsername;
             password = roleAcbPassword;
+        } else {
+        throw new IllegalArgumentException("Incorrect role found: " + role);
         }
         getDriver().get(getUrl() + "#/admin/dpManagement");
         WebDriverWait wait = new WebDriverWait(getDriver(), TIMEOUT);
