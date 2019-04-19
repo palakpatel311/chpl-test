d149ac5d989959a669577a51a18e4317f6abb9e2
Ashwin Iyer <iyerashwin14@gmail.com>
2019-04-19 15:58:01 -0400

    test: simplified editing of product

---
+++ src/test/java/gov/healthit/chpl/aqa/asserts/ViewProductPageAsserts.java
@@ -0,0 +1,70 @@
+package gov.healthit.chpl.aqa.asserts;
+
+import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.JavascriptExecutor;
+
+import cucumber.api.java.en.Then;
+import gov.healthit.chpl.aqa.pageObjects.ViewProductPage;
+import gov.healthit.chpl.aqa.stepDefinitions.Base;
+
+/**
+ * Class ViewProductPageAsserts definition.
+ */
+public class ViewProductPageAsserts extends Base {
+
+    /**
+     * Assert that product edit link exists on view version page.
+     * @param productId is the product id passed in page objects
+     */
+    @Then("^I see product edit link for product \"([^\"]*)\"$")
+    public void iSeeProductEditLink(final String productId) {
+        ViewProductPage.editProductLink(getDriver(), productId).isDisplayed();
+        assertTrue(true);
+    }
+
+    /**
+     * Assert that product split link exists on view version page.
+     * @param productId is the product id passed in page objects
+     */
+    @Then("^I see product split link for product \"([^\"]*)\"$")
+    public void iSeeProductSplitLink(final String productId) {
+        ViewProductPage.splitProductLink(getDriver(), productId).isDisplayed();
+        assertTrue(true);
+    }
+
+    /**
+     * Assert that product merge link exists on view version page.
+     * @param productId is the product id passed in page objects
+     */
+    @Then("^I see product merge link for product \"([^\"]*)\"$")
+    public void iSeeProductMergeLink(final String productId) {
+        ViewProductPage.mergeProductLink(getDriver(), productId).isDisplayed();
+        assertTrue(true);
+    }
+
+    /**
+     * Assert that "Field is required" error message is displayed for Product Name.
+     * @param expectedErrorMessage is the expected error message
+     * @param productId is the product id passed in page objects
+     */
+    @Then("^I see error message \"(.*)\" for product \"(.*)\"$")
+    public void iSeeFieldIsRequiredError(final String expectedErrorMessage, final String productId) {
+        String actualErrorMessage = ViewProductPage.productNameMissingErrorMessage(getDriver(), productId).getText();
+        assertEquals(actualErrorMessage, expectedErrorMessage);
+    }
+
+    /**
+     * Assert that product name is correct.
+     * @param productId is the product id passed in page objects
+     */
+    @Then("^I see the edited product information is recorded and updated on View Product page for product \"(.*)\"$")
+    public void editedInformationRecordedAndUpdated(final String productId) {
+        String actualProductVersion = ViewProductPage.viewProductName(getDriver(), productId).getText();
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0];", actualProductVersion);
+        assertEquals(actualProductVersion, getCurrentDate());
+    }
+}
+
+
---
+++ src/test/java/gov/healthit/chpl/aqa/stepDefinitions/ViewProductSteps.java
@@ -0,0 +1,61 @@
+package gov.healthit.chpl.aqa.stepDefinitions;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import gov.healthit.chpl.aqa.pageObjects.ViewProductPage;
+
+/**
+ * Class ViewVersionSteps definition.
+ */
+public class ViewProductSteps extends Base {
+
+    /**
+     * Get user to the View Version Page.
+     * @param developerId is Developer ID
+     * @param productId is Product ID
+     **/
+    @Given("^I navigate to the product edit page for product \"([^\"]*)\" of developer \"([^\"]*)\"$")
+    public void editVersionPage(final String productId, final String developerId) {
+        getDriver().get(getUrl() + "#/organizations/developers/" + developerId + "/products/" + productId);
+        WebDriverWait wait = new WebDriverWait(getDriver(), TIMEOUT);
+        wait.until(ExpectedConditions.visibilityOf(ViewProductPage.viewProductPageTitle(getDriver())));
+    }
+
+    /**
+     * Click Edit link of a product.
+     * @param productId is the product version id passed in page objects
+     **/
+    @And("^I click the edit link for product \"(.*)\"$")
+    public void clickEditLinkOfProduct(final String productId) {
+        ViewProductPage.editProductLink(getDriver(), productId).click();
+    }
+
+    /**
+     * Clear product name field.
+     * @param value to be entered
+     **/
+    @And("^I set the product name field to \"(.*)\"$")
+    public void setProductNameField(final String value) {
+        ViewProductPage.productNameField(getDriver()).clear();
+        ViewProductPage.productNameField(getDriver()).sendKeys(value);
+    }
+
+    /**
+     * Edit a product.
+     * @param productId is the product version id passed in page objects
+     **/
+    @And("^I edit for product \"(.*)\"$")
+    public void editProduct(final String productId) {
+        ViewProductPage.editProductLink(getDriver(), productId).click();
+        ViewProductPage.productNameField(getDriver()).clear();
+        ViewProductPage.productNameField(getDriver()).sendKeys(getCurrentDate());
+        WebElement link = ViewProductPage.editProductSaveButton(getDriver(), productId);
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", link);
+    }
+}
+
