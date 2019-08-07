d533560a426d98194828069d59ef748901117a30
Ashwin Iyer <iyerashwin14@gmail.com>
2019-08-07 05:00:49 -0400

    refactor: remove unused page object

--- src/test/java/gov/healthit/chpl/aqa/pageObjects/AnnouncementsPage.java
+++ src/test/java/gov/healthit/chpl/aqa/pageObjects/AnnouncementsPage.java
@@ -46,10 +46,6 @@ public final class AnnouncementsPage extends BasePage {
         return driver.findElement(By.xpath("//*[@id=\"main-content\"]/div/ui-view/chpl-reports-announcements/div"));
     }
 
-    public static WebElement editExistingAnnouncement(final WebDriver driver) {
-        return driver.findElement(By.xpath("//tr[2]//td[6]//button[1]"));
-    }
-
     public static WebElement deleteAnnouncement(final WebDriver driver) {
         return driver.findElement(By.xpath("//button[@id='announcement-delete']"));
     }
