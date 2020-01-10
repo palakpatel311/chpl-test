2df3d180418bc3bcb26263c8c9319b97c6c4803b
palakpatel311 <papatel@ainq.com>
2020-01-10 13:14:05 -0500

    checkstyle updated

--- src/test/java/gov/healthit/chpl/aqa/stepDefinitions/OncAcbManagementSteps.java
+++ src/test/java/gov/healthit/chpl/aqa/stepDefinitions/OncAcbManagementSteps.java
@@ -15,92 +15,98 @@ import gov.healthit.chpl.aqa.pageObjects.OncAcbManagementPage;
  */
 public class OncAcbManagementSteps extends Base {
 
-	/**
-	 * Get user to ONC ACB Management  page.
-	 */
-	@And("^I navigate to ONC ACB Management page$")
-	public void navigateToONCACBManagementPage() {
-		getDriver().get(getUrl() + "#/organizations/onc-acbs");
-		WebDriverWait wait = new WebDriverWait(getDriver(), TIMEOUT);
-		wait.until(ExpectedConditions.visibilityOf(OncAcbManagementPage.oncACBManagementPageTitle(getDriver())));
-	}
+    /**
+     * Get user to ONC ACB Management page.
+     */
+    @And("^I navigate to ONC ACB Management page$")
+    public void navigateToONCACBManagementPage() {
+        getDriver().get(getUrl() + "#/organizations/onc-acbs");
+        WebDriverWait wait = new WebDriverWait(getDriver(), TIMEOUT);
+        wait.until(ExpectedConditions.visibilityOf(OncAcbManagementPage.oncACBManagementPageTitle(getDriver())));
+    }
 
-	/**
-	 * Access ONC ACB details page.
-	 * @param acb is the name of ONC ACB
-	 */
-	@And("^I access \"([^\"]*)\" ACB details$")
-	public void iClickOnONCACBName(final String acb) {
-		OncAcbManagementPage.oncACBName(getDriver(), acb).click();
-	}
+    /**
+     * Access ONC ACB details page.
+     *
+     * @param acb
+     *            is the name of ONC ACB
+     */
+    @And("^I access \"([^\"]*)\" ACB details$")
+    public void iClickOnONCACBName(final String acb) {
+        OncAcbManagementPage.oncACBName(getDriver(), acb).click();
+    }
 
-	/**
-	 * Open the ACB edit form.
-	 */
-	@And("^I open the ACB edit form$")
-	public void editACB() {
-		WebElement link = OncAcbManagementPage.editONCACB(getDriver());
-		((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", link);
-		WebDriverWait wait = new WebDriverWait(getDriver(), TIMEOUT);
-		wait.until(ExpectedConditions.visibilityOf(OncAcbManagementPage.markRetirementStatus(getDriver())));
-	}
+    /**
+     * Open the ACB edit form.
+     */
+    @And("^I open the ACB edit form$")
+    public void editACB() {
+        WebElement link = OncAcbManagementPage.editONCACB(getDriver());
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", link);
+        WebDriverWait wait = new WebDriverWait(getDriver(), TIMEOUT);
+        wait.until(ExpectedConditions.visibilityOf(OncAcbManagementPage.markRetirementStatus(getDriver())));
+    }
 
-	/**
-	 * Retire the ACB on a specific date.
-	 * @param date set the retirement date
-	 * @throws Throwable 
-	 */
-	@When("^I mark it as retired on \"([^\"]*)\"$")
-	public void markRetired(final String date) throws Throwable {
-		Actions act=new Actions(getDriver());
-		act.moveToElement(OncAcbManagementPage.markRetirementStatus(getDriver())).click().build().perform();
-		OncAcbManagementPage.retirementDate(getDriver()).clear();
-		OncAcbManagementPage.retirementDate(getDriver()).sendKeys(date);
-		WebElement link = OncAcbManagementPage.saveONCACB(getDriver());
-		((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", link);
-	}
+    /**
+     * Retire the ACB on a specific date.
+     *
+     * @param date
+     *            set the retirement date
+     * @throws Throwable
+     */
+    @When("^I mark it as retired on \"([^\"]*)\"$")
+    public void markRetired(final String date) throws Throwable {
+        Actions act = new Actions(getDriver());
+        act.moveToElement(OncAcbManagementPage.markRetirementStatus(getDriver())).click().build().perform();
+        OncAcbManagementPage.retirementDate(getDriver()).clear();
+        OncAcbManagementPage.retirementDate(getDriver()).sendKeys(date);
+        WebElement link = OncAcbManagementPage.saveONCACB(getDriver());
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", link);
+    }
 
-	/**
-	 * Unretire an existing retired ACB.
-	 */
-	@When("^I unretire an existing retired ACB$")
-	public void iUnretireExistingRetiredACB() {
-		if(OncAcbManagementPage.markRetirementStatus(getDriver()).isSelected()) {
-			OncAcbManagementPage.retirementDate(getDriver()).clear();
-			Actions act=new Actions(getDriver());
-			act.moveToElement(OncAcbManagementPage.markRetirementStatus(getDriver())).click().build().perform();
-		} else {
+    /**
+     * Unretire an existing retired ACB.
+     */
+    @When("^I unretire an existing retired ACB$")
+    public void iUnretireExistingRetiredACB() {
+        if (OncAcbManagementPage.markRetirementStatus(getDriver()).isSelected()) {
+            OncAcbManagementPage.retirementDate(getDriver()).clear();
+            Actions act = new Actions(getDriver());
+            act.moveToElement(OncAcbManagementPage.markRetirementStatus(getDriver())).click().build().perform();
+        }
+        OncAcbManagementPage.addressFirstLine(getDriver()).clear();
+        OncAcbManagementPage.addressFirstLine(getDriver()).sendKeys("Test");
+        OncAcbManagementPage.addressCity(getDriver()).clear();
+        OncAcbManagementPage.addressCity(getDriver()).sendKeys("Test");
+        OncAcbManagementPage.addressState(getDriver()).clear();
+        OncAcbManagementPage.addressState(getDriver()).sendKeys("Test");
+        OncAcbManagementPage.addressZipCode(getDriver()).clear();
+        OncAcbManagementPage.addressZipCode(getDriver()).sendKeys("111");
+        OncAcbManagementPage.addressCountry(getDriver()).clear();
+        OncAcbManagementPage.addressCountry(getDriver()).sendKeys("Test");
+        WebElement link = OncAcbManagementPage.saveONCACB(getDriver());
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", link);
+    }
 
-		}
-		OncAcbManagementPage.addressFirstLine(getDriver()).clear();
-		OncAcbManagementPage.addressFirstLine(getDriver()).sendKeys("Test");
-		OncAcbManagementPage.addressCity(getDriver()).clear();
-		OncAcbManagementPage.addressCity(getDriver()).sendKeys("Test");
-		OncAcbManagementPage.addressState(getDriver()).clear();
-		OncAcbManagementPage.addressState(getDriver()).sendKeys("Test");
-		OncAcbManagementPage.addressZipCode(getDriver()).clear();
-		OncAcbManagementPage.addressZipCode(getDriver()).sendKeys("111");
-		OncAcbManagementPage.addressCountry(getDriver()).clear();
-		OncAcbManagementPage.addressCountry(getDriver()).sendKeys("Test");
-		WebElement link = OncAcbManagementPage.saveONCACB(getDriver());
-		((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", link);
-	}
-
-	/**
-	 * Edit ACB details to edit ACB name to new name and edit it back to original ACB name.
-	 * @param newName is new ACB name
-	 * @param oldName is original ACB name
-	 */
-	@When("^I edit ACB name to be \"([^\"]*)\" and edit it back to \"([^\"]*)\"$")
-	public void editAcbName(final String newName, final String oldName) {
-		OncAcbManagementPage.oncACBNameOnEditForm(getDriver()).clear();
-		OncAcbManagementPage.oncACBNameOnEditForm(getDriver()).sendKeys(newName);
-		OncAcbManagementPage.saveONCACB(getDriver()).click();
-		WebElement link = OncAcbManagementPage.editONCACB(getDriver());
-		((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", link);
-		OncAcbManagementPage.oncACBNameOnEditForm(getDriver()).clear();
-		OncAcbManagementPage.oncACBNameOnEditForm(getDriver()).sendKeys(oldName);
-		link = OncAcbManagementPage.saveONCACB(getDriver());
-		((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", link);
-	}
+    /**
+     * Edit ACB details to edit ACB name to new name and edit it back to
+     * original ACB name.
+     *
+     * @param newName
+     *            is new ACB name
+     * @param oldName
+     *            is original ACB name
+     */
+    @When("^I edit ACB name to be \"([^\"]*)\" and edit it back to \"([^\"]*)\"$")
+    public void editAcbName(final String newName, final String oldName) {
+        OncAcbManagementPage.oncACBNameOnEditForm(getDriver()).clear();
+        OncAcbManagementPage.oncACBNameOnEditForm(getDriver()).sendKeys(newName);
+        OncAcbManagementPage.saveONCACB(getDriver()).click();
+        WebElement link = OncAcbManagementPage.editONCACB(getDriver());
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", link);
+        OncAcbManagementPage.oncACBNameOnEditForm(getDriver()).clear();
+        OncAcbManagementPage.oncACBNameOnEditForm(getDriver()).sendKeys(oldName);
+        link = OncAcbManagementPage.saveONCACB(getDriver());
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", link);
+    }
 }
